e48b7cc3dc90601d671c452824b1dbbb2a2b2ff4
Marketing Director <nicolestark1103@gmail.com>
2023-06-12 10:53:59 -0500

    Added prompt user for password lenght and for lowercase, uppercase, numeric, and special characters

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword () {
 
   // Prompt the user for password length (between 8 & 128)
+  var passwordLength = window.prompt("How many characters would you like your password to contain? Between 8 & 128 ");
 
   // Prompt the user for lowercase, uppercase, numeric, special characters
+  var specialCharacters = window.confirm("Click OK to include special characters.");
+  var upperCase = window.confirm("Click OK to include uppercase letters.");
+  var lowerCase = window.confirm("Click OK to include lowercase letters.");
+  var numeric = window.confirm("Click OK to include numerics.");
 
   // Validate input
 
-  // Password displays to page
+  // Generate password
 
+  // Password displays to page
   return "Generated password will go here";
 }
 
