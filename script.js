d1e73df6b5ab407b25790c6f310181a39f828d92
Marketing Director <nicolestark1103@gmail.com>
2023-06-12 10:44:55 -0500

    pseudo code added to js file

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Define function generatePassword
+function generatePassword () {
+
+  // Prompt the user for password length (between 8 & 128)
+
+  // Prompt the user for lowercase, uppercase, numeric, special characters
+
+  // Validate input
+
+  // Password displays to page
+
+  return "Generated password will go here";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,4 +25,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+// When user clicks the generate button, it will call the writePassword function
 generateBtn.addEventListener("click", writePassword);
